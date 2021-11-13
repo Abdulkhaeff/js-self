14520489a96b64826cd9b36b8bcbb72d9e48f3d2
Abdulkhaeff <elbekabdulkhaev@gmail.com>
2021-11-13 16:53:45 +0300

    salommmmmmmmmm

--- js.js
+++ js.js
@@ -1,10 +1,48 @@
 
-for(let i=0;i<=100 ;i= i + 2){
-console.log(i,"Juft son");
-}
-console.log("-------------");
+// for(let i=0;i<=100 ;i= i + 2){
+// console.log(i,"Juft son");
+// }
+// console.log("-------------");
 
-for(let i=1;i<=100;i = i+2){
-    console.log(i,"Toq son");
-}
+// for(let i=1;i<=100;i = i+2){
+//     console.log(i,"Toq son");
+// }
 
+
+
+
+
+// let a = 0;
+// for(let i=0;i<=100;i++){
+// a += i;
+// }
+// console.log(a ,"hammasini yig`indisi");
+
+// let a = 0;
+// for(let i=0;i<=100;i++){
+// a = a -i
+// }
+// console.log(a);
+
+
+
+// for(let i=10;i<=20;i=i+2){
+//     console.log(i,"juft sonlar");
+// }
+
+// for(let i=21;i<=100;i=i+2){
+//     console.log(i,"toqsonlar");
+// }
+
+
+
+
+// topish = function (a) {
+//     a * 6;
+// }
+// console.log(topish(7));
+
+
+// var a =33
+// var b =22
+// console.log(a+ b + ((parseIntMath.random(100) * 100+(parseFloat('3.456456')))))
