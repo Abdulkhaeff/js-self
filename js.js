38ad71b3e647d2e5b867522e2aa0916e9ca01b2b
Abdulkhaeff <elbekabdulkhaev@gmail.com>
2021-11-17 14:21:21 +0300

    salom

--- js.js
+++ js.js
@@ -1,84 +1,233 @@
 
-// for(let i=0;i<=100 ;i= i + 2){
-// console.log(i,"Juft son");
-// }
-// console.log("-------------");
+// // for(let i=0;i<=100 ;i= i + 2){
+// // console.log(i,"Juft son");
+// // }
+// // console.log("-------------");
 
-// for(let i=1;i<=100;i = i+2){
-//     console.log(i,"Toq son");
-// }
-
-
-
-
-
-// let a = 0;
-// for(let i=0;i<=100;i++){
-// a += i;
-// }
-// console.log(a ,"hammasini yig`indisi");
-
-// let a = 0;
-// for(let i=0;i<=100;i++){
-// a = a -i
-// }
-// console.log(a);
-
-
-
-// for(let i=10;i<=20;i=i+2){
-//     console.log(i,"juft sonlar");
-// }
-
-// for(let i=21;i<=100;i=i+2){
-//     console.log(i,"toqsonlar");
-// }
+// // for(let i=1;i<=100;i = i+2){
+// //     console.log(i,"Toq son");
+// // }
 
 
 
 
-// topish = function (a) {
-//     a * 6;
-// }
-// console.log(topish(7));
+
+// // let a = 0;
+// // for(let i=0;i<=100;i++){
+// // a += i;
+// // }
+// // console.log(a ,"hammasini yig`indisi");
+
+// // let a = 0;
+// // for(let i=0;i<=100;i++){
+// // a = a -i
+// // }
+// // console.log(a);
 
 
-// var a =33
-// var b =22
-// console.log(a+ b + ((parseIntMath.random(100) * 100+(parseFloat('3.456456')))))
 
-// <p id="demo"></p>
+// // for(let i=10;i<=20;i=i+2){
+// //     console.log(i,"juft sonlar");
+// // }
 
-// try {
-//   adddlert("Welcome guest!");
-// }
-// catch(err) {
-//   document.getElementById("demo").innerHTML = err.message;
-// }
-// function myFunction() {
-//     const message = document.getElementById("p01");
-//     message.innerHTML = "";
-//     let x = document.getElementById("demo").value;
-//     try {
-//       if(x == "") throw "empty";
-//       if(isNaN(x)) throw "not a number";
-//       x = Number(x);
-//       if(x < 5) throw "too low";
-//       if(x > 10) throw "too high";
-
-// function isArray(myArray) {
-//     return myArray.constructor === Array;
-//   }
+// // for(let i=21;i<=100;i=i+2){
+// //     console.log(i,"toqsonlar");
+// // }
 
 
-// Function for Method ceil
-
-// function ceil(a) {
-//     return (a + 0.5).toFixed(0);
-// }
-// console.log(ceil(1.001));
 
 
+// // topish = function (a) {
+// //     a * 6;
+// // }
+// // console.log(topish(7));
+
+
+// // var a =33
+// // var b =22
+// // console.log(a+ b + ((parseIntMath.random(100) * 100+(parseFloat('3.456456')))))
+
+// // <p id="demo"></p>
+
+// // try {
+// //   adddlert("Welcome guest!");
+// // }
+// // catch(err) {
+// //   document.getElementById("demo").innerHTML = err.message;
+// // }
+// // function myFunction() {
+// //     const message = document.getElementById("p01");
+// //     message.innerHTML = "";
+// //     let x = document.getElementById("demo").value;
+// //     try {
+// //       if(x == "") throw "empty";
+// //       if(isNaN(x)) throw "not a number";
+// //       x = Number(x);
+// //       if(x < 5) throw "too low";
+// //       if(x > 10) throw "too high";
+
+// // function isArray(myArray) {
+// //     return myArray.constructor === Array;
+// //   }
+
+
+// // Function for Method ceil
+
+// // function ceil(a) {
+// //     return (a + 0.5).toFixed(0);
+// // }
+// // console.log(ceil(1.001));
+
+
+// // var f = function() {
+
+// //   console.log(1);
+
+// // }
+// // var execute = function(f) {
+
+// //     setTimeout(f, 1000);
+  
+// //   }
+  
+// //   execute(f);
+  
+// //   f = function() {
+  
+// //     console.log(2);
+  
+// //   }
+// // console.log(null || 1 || undefined) // 1
+
+// // function logName(name) {
+// //     let n = name || Mark
+// //     console.log(n)
+// // }
+// // function giveMeX(showX){
+// //     if(showX){
+// //         var x = 5
+// //     }
+// //     return x
+// // }
+
+// // console.log(giveMeX(false))
+// // console.log(giveMeX(true))
+
+
+// // function giveMeX(showX){
+// //     if(showX){
+// //         let x = 5
+// //     }
+// //     return x
+// // }
+
+// // function giveMeY(showY){
+// //     if(showY){
+// //         let y = 5
+// //     }
+// //     return y
+// // }
+// // console.log(giveMeX());
+// // console.log(!![].length)
+
+
+
+// // console.log(y)
+// // y = 1
+// // console.log(y)
+// // console.log(greet('Mark'))
+
+// // function greet(name) {
+// //     return 'Hello ' + name + '!'
+// // }
+
+// // var y;
+
+// // document.form[0].submit();
+// // document.form[0].submit();
+
+
+// // hoistedFunction();  
+
+// // function hoistedFunction(){ 
+// //   console.log(" Hello world! ");
+// // }
+
+
+// // var obj = { name: "Vivek", surname: "Bisht" };
+
+// // var obj2 = obj;
+
+// // console.log(obj2);
+
+
+
+
+// // ( 1 )  Number
+
+// false && true ;true || false  ;  !true || false  ;  !!false && true
+// //         ( 2 ) 
+// // // let numOne = Math.round(1.005 * 100) / 100;
+// // // console.log(numOne);
+
+//         ( 3 )
+
+// // let problem = 0.1 + 0.2 === 0.3;
+// // console.log(problem);
+
+//         ( 4 ) 
+
+// // console.log(Number(25 + 'Hello !'));
+// // console.log(isNaN(25 + 'Hello !'));
+//          ( 5 ) Array
+
+// // let valueOne = '150.85px'
+// // console.log(parseInt(valueOne));
+// // console.log(parseFloat(valueOne));
+
+//         ( 6 ) 
+
+// // var vegetables = ['Karam', 'Olma', 'Anor', 'Behi'];
+// // arrayni str type ga otkazing
+
+// // var str1 = vegetables.toString();
+// // var str2 = vegetables.join();
+//         ( 7 ) 
+
+// // kaysi metod bilan ikkta array ni ulaydi ?
+
+//          ( 8 )  
+
+// // str type li varrayble ni arrayga otkazing
+
+//         ( 9 ) Object
+
+// // object ni ichiga yangi element koshish va objectni ichidagi elementnin ochirish
+
+//          ( 10) 
+
+// // var vegetables = ['Salom', 'Qalesiz', 'Kimsiz', 'Nimaga'];
+// // arrayni ichidagi 3 indexli element ni boshaka malumotga almashtiring
+
+//          ( 11 ) 
+
+// // 'this key word'di nima kiladi
+//          ( 12 )  Funktion
+
+// // function show(){
+// //     console.log(Hello);
+// //     function show1(){
+// //         console.log(Hello World);
+// //     }
+// // }
+// // show();
+// // show1();
+
+// // hatoni toping va nima ?
+
+//          (13)
+// NaN nima? Bu qiymat qanday turdagi? O'zgaruvchining qiymati NaN ekanligini qanday aniqlash mumkin?
+//          (14)
 // var f = function() {
 
 //   console.log(1);
@@ -97,68 +246,21 @@
 //     console.log(2);
   
 //   }
+//         (15)
+// console.log(Boolean(undefined))
+// javob nima ciqadi
+//         (16)
+// console.log(!!undefined)
+// javob nima chiqadi
+//         (17)
 // console.log(null || 1 || undefined) // 1
 
 // function logName(name) {
 //     let n = name || Mark
 //     console.log(n)
 // }
-// function giveMeX(showX){
-//     if(showX){
-//         var x = 5
-//     }
-//     return x
-// }
-
-// console.log(giveMeX(false))
-// console.log(giveMeX(true))
-
-
-// function giveMeX(showX){
-//     if(showX){
-//         let x = 5
-//     }
-//     return x
-// }
-
-// function giveMeY(showY){
-//     if(showY){
-//         let y = 5
-//     }
-//     return y
-// }
-// console.log(giveMeX());
-// console.log(!![].length)
-
-
-
-// console.log(y)
-// y = 1
-// console.log(y)
-// console.log(greet('Mark'))
-
-// function greet(name) {
-//     return 'Hello ' + name + '!'
-// }
-
-// var y;
-
-// document.form[0].submit();
-// document.form[0].submit();
-
-
-// hoistedFunction();  
-
-// function hoistedFunction(){ 
-//   console.log(" Hello world! ");
-// }
-
-
-// var obj = { name: "Vivek", surname: "Bisht" };
-
-// var obj2 = obj;
-
-// console.log(obj2);
+// javob nima chiqadi?      il chiqadi
+//         (18)
 
 
 
