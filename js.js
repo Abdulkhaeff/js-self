a75b77f9ba9cc2bc5b4994b2d84782fac85d0f1d
Abdulkhaeff <elbekabdulkhaev@gmail.com>
2021-11-21 19:57:26 +0300

    homework

--- js.js
+++ js.js
@@ -364,13 +364,16 @@
 
 
 
-var o = 0;
-var a = 0;
-var fruits = ['Olma','anor','nok','behi','bAnan'];
-var fruits2= fruits.join('');
-// console.log(fruits2);
-for (let i= 0; i < fruits2.length; i++) {
-   ( fruits2[i]==='o'||fruits2[i]=== 'O' )&& o++
-   (fruits2[i]==='a'||fruits2[i]==='A')&& a++
-}
-console.log(o,a);
+// var o = 0;
+// var a = 0;
+// var fruits = ['Olma','anor','nok','behi','bAnan'];
+// var fruits2= fruits.join('');
+// // console.log(fruits2);
+// for (let i= 0; i < fruits2.length; i++) {
+//    ( fruits2[i]==='o'||fruits2[i]=== 'O' )&& o++
+//    (fruits2[i]==='a'||fruits2[i]==='A')&& a++
+// }
+// console.log(o,a);
+
+
+
